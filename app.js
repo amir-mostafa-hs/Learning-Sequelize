79ef27720cda64b3af3a8b60a7a7e1d25eee7bee
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-06-22 12:06:24 +0430

    connect to data base and test connected

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+// conect to data base with sequelize
+const Sequelize = require("sequelize");
+const db = new Sequelize("codegig", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
+
+// test conect db
+db.authenticate()
+  .then(() => console.log("Database connected..."))
+  .catch((error) => console.log(error));
+
 // create express app
 const app = express();
 
